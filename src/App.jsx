33b08a3bb7f6b71c05819ce795f04b2a56fd3f61
Brian Adams <brianadamsmaino68@gmail.com>
2023-11-18 07:44:41 +0100

    Limit rate error, would find a solution

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,35 @@ const App = () => {
   const [username, setUsername] = useState('');
   const [userData, setUserData] = useState(null);
 
-  const fetchUserData = async () => {
+  const searchUserByName = async (name) => {
     try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
+      const token = import.meta.env.VITE_PERSONAL_TOKEN
+      const perPage = 25;
+    const response = await fetch(`https://api.github.com/search/users?q=${encodeURIComponent(name)}&per_page=${perPage}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
       const data = await response.json();
-      setUserData(data);
+      console.log(data);
+  
+      if (response.ok) {
+        const users = data.items;
+        console.log(users);
+      } else {
+        console.error('Error:', data.message);
+      }
     } catch (error) {
-      console.error('Error fetching user data:', error);
+      console.error('Error fetching data:', error);
     }
   };
+  
+
+  searchUserByName('John Doe');  
 
   return (
     <div className="container mx-auto mt-8 text-center">
+      <img className='h-40 w-40' src="/src/assets/git-icon.png" alt="" />
       <h1 className="text-4xl font-bold mb-4">GitHub Profile Viewer</h1>
       <div className="mb-4">
         <input
@@ -27,23 +44,23 @@ const App = () => {
           onChange={(e) => setUsername(e.target.value)}
           className="border p-2"
         />
-        <button onClick={fetchUserData} className="ml-2 bg-blue-500 text-white p-2">
+        <button onClick={searchUserByName} className="ml-2 bg-blue-500 text-white p-2">
           Search
         </button>
       </div>
-      {userData && (
-        <div>
-          <img
-            src={userData.avatar_url}
-            alt={`${userData.login}'s avatar`}
-            className="rounded-full mx-auto mb-4"
-          />
-          <h2 className="text-2xl font-bold">{userData.login}</h2>
-          <p>{userData.bio}</p>
-          <p className="mt-2">
-            Followers: {userData.followers} | Following: {userData.following}
-          </p>
-        </div>
+      {userData ? (
+        userData.map((user) => (
+          <div key={user.id}>
+            <img src={user.avatar_url} alt={`${user.login}'s avatar`} className="rounded-full mx-auto mb-4" />
+            <h2 className="text-2xl font-bold">{user.login}</h2>
+            <p>{user.bio}</p>
+            <p className="mt-2">
+              Followers: {user.followers} | Following: {user.following}
+            </p>
+          </div>
+        ))
+      ) : (
+        <p>No user data available</p>
       )}
     </div>
   );
